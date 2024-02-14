1afed5d4f3f65c74f37eca333e97318bfc68c7de
opsejf <yellowotters1@gmail.com>
2024-02-14 13:42:37 +0000

    about us changes

--- groupproject/src/components/aboutUs.js
+++ groupproject/src/components/aboutUs.js
@@ -4,11 +4,12 @@ import '../styles/aboutUs.css';
 import male from '../Images/maletemplate.jpg';
 import female from '../Images/femaletemplate.jpg';
 import woman1 from '../Images/woman1.jpg';
+import man2 from '../Images/man2.jfif';
 
-function person(props){
+function contact(){
     return(
         <div>
-            <img src='groupproject\src\Images\maletemplate.jpg'/>
+            <img src=''/>
         </div>
     )
 }
@@ -22,17 +23,17 @@ let AboutUs = () => {
             <br />
             <br />
             <h2>Meet The Team!</h2>
-            <h3>Person One</h3>
+            <h3>Properties Manager - Adam Thompson</h3>
             <img className="employee" src={male}></img>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <h3>Person Two</h3>
+            <h3>Estate Advisor - Leanne Smith</h3>
             <img className="employee" src={woman1}></img>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <h3>Person Three</h3>
+            <h3>Online Manager - Tracy Blythe</h3>
             <img className="employee" src={female}></img>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <h3>Person Four</h3>
-            <img className="employee" src={male}></img>
+            <h3>Realtor - Gary Norman</h3>
+            <img className="employee" src={man2}></img>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <h3>Person Five</h3>
         </div>
